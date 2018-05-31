228545e1ecf0c81e013281490a3fe66a6e3d353a
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2018-05-31 18:01:41 +0100

    add field spectra, corrected uav refl,  several bits of analysis

--- uav_timeseries.py
+++ uav_timeseries.py
@@ -5,12 +5,17 @@ Mask: save shapefile in EPSG:32623, then:
 gdal_rasterize -a_srs EPSG:32623 -a id -tr 0.05 0.05 -at good_area_2017-07-24.shp good_area_2017-07-24_3.tif
 gdalwarp -te 310895 7446509 311219 7446839 good_area_2017-07-24_3.tif good_area_2017-07-24_3_common.tif
 
+For discrete colourmap information this is a good source:
+https://gist.github.com/jakevdp/91077b0cae40f8f8244a
+
 """
 import xarray as xr
 import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib as mpl
+import pyproj
+import numpy as np
 
 import georaster
 
@@ -20,69 +25,73 @@ uav_modis_y = -2507935
 
 
 ## BBA values
-# Run ~/projects/weathering_crust/analyse_field_spectra.py first!
+log_sheet = pd.read_csv('/scratch/field_spectra/temporal_log_sheet.csv')
+bba = pd.read_csv('/scratch/field_spectra/BBA.txt')
+# Mangle BBA.txt to get date/time information out 
+info = bba['RowName'].str.split('_', 3, expand=True)
+info.columns = ['Day','Month','Site','fill']
+info['Month'] = info['Month'].astype(float)
+info['Day'] = info['Day'].astype(float)
+info['Site'] = info['Site'].astype(str)
+info = info.drop('fill', axis=1)
+bbas = pd.concat([bba,info], axis=1)
+# BBA.txt contains albedo value for every single replicate, so generate
+# means from each set of replicate measurements
+bbas_mean = bbas.groupby(['Day','Month','Site']).mean()
+bbas_mean = bbas_mean.reset_index()
+bbas_log = bbas_mean.merge(log_sheet,on=['Day','Month','Site'])
+
+# Calculate daily mean BBA from Fieldspec samples
 # Index corresponding to temporal samples
 daily_bba_asd = bbas_log.BBA_list.groupby(bbas_log.Day).mean()
 ix = [pd.datetime(2017,7,13),pd.datetime(2017,7,14),pd.datetime(2017,7,15),pd.datetime(2017,7,21),pd.datetime(2017,7,22),pd.datetime(2017,7,23),pd.datetime(2017,7,24),pd.datetime(2017,7,25)]
 daily_bba_asd.index = ix
 
-
-
-def discrete_cmap(N, base_cmap=None):
-    """Create an N-bin discrete colormap from the specified input map"""
-
-    # https://gist.github.com/jakevdp/91077b0cae40f8f8244a
-
-    # Note that if base_cmap is a string or None, you can simply do
-    #    return plt.cm.get_cmap(base_cmap, N)
-    # The following works for string, None, or a colormap instance:
-
-    base = plt.cm.get_cmap(base_cmap)
-    color_list = base(np.linspace(0, 1, N))
-    cmap_name = base.name + str(N)
-    return base.from_list(cmap_name, color_list, N)
-
-
+# Not sure what this does...
 # grouped = spectra.groupby('label').mean().drop(labels='numeric_label',axis=1).T
 # grouped.index = [475,560,668,717,840]
 
-uav = xr.open_mfdataset('/scratch/UAV/uav2017_common_grid_nc/*class.nc',
-	concat_dim='time', chunks={'y':2000, 'x':2000})
 
-uav['time'] = [dt.datetime(2017,7,15),
-	dt.datetime(2017,7,17),
+# Read all UAV images into one multi-temporal frame.
+uav_times = [dt.datetime(2017,7,15),
 	dt.datetime(2017,7,20),
 	dt.datetime(2017,7,21),
 	dt.datetime(2017,7,22),
 	dt.datetime(2017,7,23),
 	dt.datetime(2017,7,24)
 	]
+uav = xr.open_mfdataset('/scratch/UAV/uav2017_commongrid_bandcorrect/*class.nc',
+	concat_dim='time', chunks={'y':2000, 'x':2000})
+# Set up the time coordinate.
+uav['time'] = uav_times 
 
-# Drop snowy mosaic
-uav = uav.drop(labels=(pd.datetime(2017, 7, 17)), dim='time')
+uav_refl = xr.open_mfdataset('/scratch/UAV/uav2017_commongrid_bandcorrect/*commongrid.nc',
+	concat_dim='time', chunks={'y':2000, 'x':2000})
+# Set up the time coordinate.
+uav_refl['time'] = uav_times 
+# Correct using ground-UAV comparisons from compare_hcrf_uav.py
+uav_refl['Band1'] -= 0.17
+uav_refl['Band2'] -= 0.18
+uav_refl['Band3'] -= 0.15
+uav_refl['Band4'] -= 0.16
+uav_refl['Band5'] -= 0.1
 
+## Create a categorical colourmap.
 vals = [0, 1, 2, 3, 4, 5, 6]
 cmap = mpl.colors.ListedColormap(['#000000','#08519C','#FFFFFF', '#C6DBEF', '#FDBB84', '#B30000', '#762A83'])
-norm = mpl.colors.BoundaryNorm(vals, cmap.N)
-
-uav.classified.sel(time='2017-07-23').plot(cmap=cmap, vmin=0, vmax=7)
-# cb = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
-#                                 norm=norm,
-#                                 spacing='uniform',
-#                                 orientation='horizontal',
-#                                 extend='neither',
-#                                 ticks=vals)
+# Example of use: uav.classified.sel(time='2017-07-23').plot(cmap=cmap, vmin=0, vmax=7)
 
 
-total_px = uav.classified.where(uav.classified.notnull()).count(dim=('y','x'))
-
-
-# Mask to delimit 'good' area of 2017-07-24 flight.
+## Load mask to delimit 'good' area of 2017-07-24 flight.
 msk = georaster.SingleBandRaster('/scratch/UAV/uav2017_common_grid_nc/good_area_2017-07-24_3_common.tif')
 
-# Compare distribution within mask area for each image, to see if statistically representative
-# Essentially, the mask area is flight number 1 - it's flight number 2 that failed.
+## Calculate total pixels available and classified
+total_px = uav.classified.where(uav.classified.notnull()).count(dim=('y','x'))
+total_px_msk = uav.classified.where(msk.r > 0).where(uav.classified.notnull()).count(dim=('y','x'))
 
+
+## Ad sentinel-2 data!
+# Check msk orientation!
 
 ## Summary statistics of surface class change through time
 uav = uav.fillna(-1)
@@ -97,8 +106,15 @@ for t in uav.time:
 	store_msk[t.values] = values_msk
 
 keys = ['NaN', 'Unknown', 'Water', 'Snow', 'Clean Ice', 'Lbio', 'Hbio', 'Cryoconite']
+
+# Whole-area statistics
 valspd = pd.DataFrame.from_dict(store, orient='index')
 valspd.columns = keys
+valspd = valspd.sort_index()
+valspd['Unknown'] = valspd['Unknown'] + valspd['NaN']
+valspd = valspd.drop(labels='NaN',axis=1)
+
+# Statistics just for masked area
 valspdm = pd.DataFrame.from_dict(store_msk, orient='index')
 valspdm.columns = keys
 valspdm = valspdm.sort_index()
@@ -106,22 +122,48 @@ valspdm['Unknown'] = valspdm['Unknown'] + valspdm['NaN']
 valspdm = valspdm.drop(labels='NaN',axis=1)
 
 
-## Albedo
+## Extract MOD10A1 Albedo for this location
 mod10 = xr.open_dataset('/scratch/MOD10A1.006.SW/MOD10A1.2017.006.reproj500m.nc')
 modalb = mod10.Snow_Albedo_Daily_Tile.where(mod10.Snow_Albedo_Daily_Tile < 100) \
-	.sel(X=-190804,Y=-2508152,method='nearest') * 0.01
-uavalb = uav.albedo.where(uav.albedo > 0).mean(dim=('x','y'))
+	.sel(X=uav_modis_x, Y=uav_modis_y, method='nearest') * 0.01
 
+## Also, b01 refl
+mod09 = xr.open_dataset('/scratch/MOD09GA.006.FB/MOD09GA.2017.006.epsg3413_b1234q.nc')
+mod09b01 = mod09.sur_refl_b01_1.sel(X=uav_modis_x, Y=uav_modis_y, method='nearest')
+
+
+## Calculate daily mean albedo from uav	
+uavalb = uav.albedo.where(uav.albedo > 0).mean(dim=('x','y'))
+uavalbm = uav.albedo.where(msk.r > 0).where(uav.albedo > 0).mean(dim=('x','y'))
+
+
+##  Extract UAV albedos at each temporal ground sampling location
+# First convert site coordinates to UTM (only for the temporal sites, numbered 1:5)
+all_gcps = pd.read_csv('/home/at15963/Dropbox/work/data/field_processed_2017/pixel_gcps_kely_formatted.csv', index_col=0)
+temporal_gcps = {}
+utm = pyproj.Proj('+init=epsg:32623')
+for	n in np.arange(1,6):
+	gcp = all_gcps.loc['GCP%s' %n]
+	utmx, utmy = utm(gcp.lon, gcp.lat)
+	# Approximately identify the reflectance measurement center - move towards camp
+	utmx += 0.4
+	utmy -= 0.4
+	temporal_gcps[n] = {'x':utmx, 'y':utmy}
+temporal_gcps = pd.DataFrame(temporal_gcps).T
 
 uav_alb_sto = {}
 for ix,row in temporal_gcps.iterrows():
 	# pixels are 0.05 m
 	x_slice = slice(row.x-(0.05*4),row.x+(0.05*4))
 	y_slice = slice(row.y-(0.05*4),row.y+(0.05*4))
-	alb = albedo.sel(x=x_slice, y=y_slice).load().median(dim=('x','y'))
+	alb = uav.albedo.sel(x=x_slice, y=y_slice).load().median(dim=('x','y'))
 	uav_alb_sto[ix] = alb.to_pandas()
-uavalb_px_new = pd.DataFrame.from_dict(uav_alb_sto)
+uavalb_px = pd.DataFrame.from_dict(uav_alb_sto)
 
+# Add daily destructive sampling locations to this logic too.
+
+
+## Plot time series of albedo comparison.
 plt.figure()
 modalb.plot(marker='o', linestyle='none', label='MODIS 500 m', alpha=0.7)
 uavalb.plot(marker='o', linestyle='none', label='UAV whole area', alpha=0.7)
@@ -131,34 +173,49 @@ plt.legend()
 plt.xlim('2017-07-12', '2017-07-27')
 
 
-flights = {
-'20170715':'uav_20170715_refl_5cm_albedo.tif',
-'20170717':'uav_20170717_refl_5cm_albedo.tif',
-'20170720':'uav_20170720_refl_5cm_albedo.tif',
-'20170721':'uav_20170721_refl_5cm_albedo.tif',
-'20170722':'uav_20170722_refl_5cm_albedo.tif',
-'20170723':'uav_20170723_refl_nolenscorr_5cm_albedo.tif',
-'20170724':'uav_20170724_refl_5cm_v2_albedo.tif'
-}
-# Now pull out values from each flight
-alb_store = {}
-uav_window = 9
-for flight in flights:
+## Plot time series of reflectance comparison
+uavred = uav_refl.Band3.where(uav_refl.Band3 > 0).mean(dim=('x','y'))
 
-	im = georaster.SingleBandRaster('/scratch/UAV/uav2017_common_grid_nc/%s' %flights[flight],
-		load_data=False)
 
-	for ix, gcp in temporal_gcps.iterrows():
-		val, win = im.value_at_coords(gcp.x, gcp.y, window=uav_window, 
-			return_window=True, reducer_function=np.nanmedian)
+plt.figure(figsize=(4,9))
+n = 1
+ytick_locs = np.array([0, 5e5, 1e6, 1.5e6, 2e6, 2.5e6])
+ytick_labels = (ytick_locs * (0.05**2)).astype(int) #sq m
+for scene in uav.time:
+	plt.subplot(6, 1, n)
+	uavh = uav.sel(time=scene)
+	if pd.Timestamp(scene.values) == pd.datetime(2017, 7, 24):
+		uavh_al = uavh.albedo.where(msk.r > 0)
+		uavh_cl = uavh.classified.where(msk.r > 0)
+	else:
+		uavh_al = uavh.albedo
+		uavh_cl = uavh.classified
 
-		# Standard deviation in each band
-		std = np.std(win)
+	uavh_al.where(uavh_cl == 5) \
+		.plot.hist(bins=50, range=(0,1), alpha=0.7, label='High Biomass', color='#B30000')
+	uavh_al.where(uavh_cl == 4) \
+		.plot.hist(bins=50, range=(0,1), alpha=0.7, label='Low Biomass', color='#FDBB84')
+	uavh_al.where(uavh_cl == 3) \
+		.plot.hist(bins=50, range=(0,1), alpha=0.7, label='Clean Ice', color='#4292C6')
+	uavh_al.where(uavh_cl == 2) \
+		.plot.hist(bins=50, range=(0,1), alpha=0.7, label='Snow', color='black')
+	plt.title(pd.Timestamp(scene.values).strftime('%Y-%m-%d'))
+	plt.yticks(ytick_locs, ytick_labels)
+	plt.ylabel('Area (sq. m)')
+	plt.xlim(0.1,0.8)
+	plt.xlabel('Albedo')
+	n += 1
+plt.tight_layout()
 
-		spec_id = flight[-2:] + '_7_S' + str(ix)
-		alb_store[spec_id] = {'med':val, 'std':std}
+"""
+Other logic to add:
 
-	im = None
+Histograms of whole area and common area through time
+	- histogram of albedo just for bare ice, low biomass, etc.
+	- Might bring performance ramifications for radiometric calibration and for classification strategy.
+
+"""
+
 
 
 ## Blob detection
@@ -175,3 +232,6 @@ for flight in flights:
 # blobs_doh = blob_doh(toblob, max_sigma=30, threshold=.01)
 # # Compute radii in the 3rd column.
 # blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
+
+
+
